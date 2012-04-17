4ab150d7f09c2ebf8af6f77dd0141502ee674d94
Eugene <inasyrov@yandex.ru>
2012-04-17 21:26:06 +0600

    cart controller updated

--- protected/controllers/CartController.php
+++ protected/controllers/CartController.php
@@ -65,7 +65,9 @@ class CartController extends CController
 					$transaction->commit();
 
 					// Перенаправляем на страницу заказа
-
+					$this->redirect('order/view', array(
+						'id' => $order->id,
+					));
 				}
 
 			}
--- themes/classic/views/order/view.php
+++ themes/classic/views/order/view.php
@@ -7,40 +7,90 @@
 	<?php if ($order->paid == 1): ?>, оплачен<?php else: ?><?php endif; ?>
 </h1>
 
-<?php foreach ($order->purchases as $purchase): ?>
-<tr>
-	<td class="image">
-		<?php if (count($purchase->product->images)): ?>
-		<a href="<?php echo $purchase->product->url; ?>">
-			<?php echo $purchase->product->images[0]->getImage(50,50,$purchase->product->name); ?>
-		</a>
-		<?php endif; ?>
-	</td>
+<table id="purchases">
+	<?php foreach ($order->purchases as $purchase): ?>
+	<tr>
+		<td class="image">
+			<?php if (count($purchase->product->images)): ?>
+			<a href="<?php echo $purchase->product->url; ?>">
+				<?php echo $purchase->product->images[0]->getImage(50, 50, $purchase->product->name); ?>
+			</a>
+			<?php endif; ?>
+		</td>
 
-	<td class="name">
-		<a href="<?php echo $purchase->product->url; ?>"><?php CHtml::encode($purchase->product->name); ?></a>
-		<?php echo CHtml::encode($purchase->variant_name); ?>
-	</td>
+		<td class="name">
+			<a href="<?php echo $purchase->product->url; ?>"><?php CHtml::encode($purchase->product->name); ?></a>
+			<?php echo CHtml::encode($purchase->variant_name); ?>
+		</td>
 
-	<td class="price">
-		<?php echo Yii::app()->money->convert($purchase->price); ?>
-		<?php echo Yii::app()->money->current->sign; ?>
-	</td>
+		<td class="price">
+			<?php echo Yii::app()->money->convert($purchase->price); ?>
+			<?php echo Yii::app()->money->current->sign; ?>
+		</td>
 
-	<td class="amount">
-		&times; <?php echo $purchase->amount ?>
-		<?php echo Yii::app()->params['units']; ?>
-	</td>
+		<td class="amount">
+			&times; <?php echo $purchase->amount ?>
+			<?php echo Yii::app()->params['units']; ?>
+		</td>
 
-	<td class="price">
-		<?php echo Yii::app()->money->convert($purchase->price*$purchase->amount); ?>
-		<?php echo Yii::app()->money->current->sign; ?>
-	</td>
-</tr>
-<?php endforeach; ?>
+		<td class="price">
+			<?php echo Yii::app()->money->convert($purchase->price * $purchase->amount); ?>
+			<?php echo Yii::app()->money->current->sign; ?>
+		</td>
+	</tr>
+	<?php endforeach; ?>
 
+	<?php if ($order->discount > 0): ?>
+	<tr>
+		<th class="image"></th>
+		<th class="name">скидка</th>
+		<th class="price"></th>
+		<th class="amount"></th>
+		<th class="price">
+			<?php $order->discount ?>
+			%
+		</th>
+	</tr>
+	<?php endif; ?>
 
+	<?php if (!$order->separate_delivery && $order->delivery_price > 0): ?>
+	<tr>
+		<td class="image"></td>
+		<td class="name">
+			<?php echo CHtml::encode($delivery->name); ?>
+		</td>
+		<td class="price"></td>
+		<td class="amount"></td>
+		<td class="price">
+			<?php echo Yii::app()->money->convert($order->delivery_price); ?>
+			<?php echo Yii::app()->money->current->sign; ?>
+		</td>
+	</tr>
+	<?php endif; ?>
 
+	<tr>
+		<th class="image"></th>
+		<th class="name">итого</th>
+		<th class="price"></th>
+		<th class="amount"></th>
+		<th class="price">
+			<?php echo Yii::app()->money->convert($order->total_price); ?>
+			<?php echo Yii::app()->money->current->sign; ?>
+		</th>
+	</tr>
 
-
- 
+	<?php if ($order->separate_delivery): ?>
+	<tr>
+		<td class="image"></td>
+		<td class="name">
+			<?php echo CHtml::encode($order->delivery->name); ?>
+		</td>
+		<td class="price"></td>
+		<td class="amount"></td>
+		<td class="price">
+			<?php echo Yii::app()->money->convert($order->delivery_price); ?>
+			<?php echo Yii::app()->money->current->sign; ?>
+		</td>
+	</tr>
+	<?php endif; ?>
+</table>
